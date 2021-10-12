8c65381f2d2466b8d7019ba23c53952897f9e5f8
GermanDZ <germ@ndz.com.ar>
2021-10-12 16:22:46 +0200

    Fix mqtt discovery

--- busing-bridge/run.sh
+++ busing-bridge/run.sh
@@ -5,14 +5,12 @@ if ! bashio::services.available "mqtt"; then
     exit -1
 else
     bashio::log.info "MQTT service found, fetching server detail ..."
-    bashio::log.info "MQTT server not found, auto-discovering ..."
     MQTT_HOST=$(bashio::services mqtt "host")
     MQTT_PORT=$(bashio::services mqtt "port")
     MQTT_SSL=$(bashio::services mqtt "ssl")
     MQTT_USERNAME=$(bashio::services mqtt "username")
     MQTT_PASSWORD=$(bashio::services mqtt "password")
-    bashio::log.info "Received user: '$MQTT_USER' for MQTT at '$MQTT_HOST:$MQTT_PORT'!"
-    fi
+    bashio::log.info "Received user: '$MQTT_USERNAME' for MQTT at '$MQTT_HOST:$MQTT_PORT'!"
 fi
 
 ruby bridge.rb
