dfc22da8b31f8fc64d5701cfa9e0cc734761d59f
GermanDZ <germ@ndz.com.ar>
2021-10-12 16:16:32 +0200

    Fix reading MQTT service config

--- busing-bridge/run.sh
+++ busing-bridge/run.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/with-contenv bashio
 
-MQTT_HOST=$(bashio::services mqtt "host")
-MQTT_PORT=$(bashio::services mqtt "port")
-MQTT_SSL=$(bashio::services mqtt "ssl")
-MQTT_USERNAME=$(bashio::services mqtt "username")
-MQTT_PASSWORD=$(bashio::services mqtt "password")
+if ! bashio::services.available "mqtt"; then
+    bashio::log.error "No internal MQTT service found. Please install Mosquitto broker"
+    exit -1
+else
+    bashio::log.info "MQTT service found, fetching server detail ..."
+    bashio::log.info "MQTT server not found, auto-discovering ..."
+    MQTT_HOST=$(bashio::services mqtt "host")
+    MQTT_PORT=$(bashio::services mqtt "port")
+    MQTT_SSL=$(bashio::services mqtt "ssl")
+    MQTT_USERNAME=$(bashio::services mqtt "username")
+    MQTT_PASSWORD=$(bashio::services mqtt "password")
+    bashio::log.info "Received user: '$MQTT_USER' for MQTT at '$MQTT_HOST:$MQTT_PORT'!"
+    fi
+fi
 
 ruby bridge.rb
